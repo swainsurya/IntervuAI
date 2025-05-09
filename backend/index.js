62e75bd383e7161847b64d1b877112a0b476c163
surya <swainsuryakanta97@gmail.com>
2025-05-09 08:39:43 +0530

    cors policy added

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ import "dotenv/config";
 import { DBConnection } from "./api/lib/connectDatabase.js";
 import authRouter from "./api/routes/auth.routes.js";
 import interviewRouter from "./api/routes/interview.routes.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 // db connection
 DBConnection();
+app.use(cors());
 app.use(express.json());
 
 // routing in server 
